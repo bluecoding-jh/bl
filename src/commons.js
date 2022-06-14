7a0122e6cd3f76c654a1a8f0ad84e3dd4bd1f934
bluecoding-jh <jonghwan0805@gmail.com>
2022-06-14 23:28:19 +0900

    init code

--- src/commons.js
+++ src/commons.js
@@ -1,7 +1,7 @@
 const Commons = {
   Functions: {
     uuidv4: (stringFormat = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx') => {
-      return stringFormat.replace( /[xy]/g, function (c) {
+      return stringFormat.replace(/[xy]/g, function (c) {
         const r = Math.random() * 16 | 0,
           v = c == 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
@@ -11,10 +11,13 @@ const Commons = {
       if (obj === null || typeof obj !== 'object') {
         return obj;
       }
+
       const result = Array.isArray(obj) ? [] : {};
+
       for (let key of Object.keys(obj)) {
         result[key] = Commons.Functions.cloneDeep(obj[key]);
       }
+
       return result;
     },
     getToday: () => {
@@ -22,10 +25,10 @@ const Commons = {
       const dd = String(today.getDate()).padStart(2, '0');
       const mm = String(today.getMonth() + 1).padStart(2, '0');
       const yyyy = today.getFullYear();
+
       return yyyy + mm + dd;
     }
   }
 }
 
 export default Commons;
-    
