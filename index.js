262879f26fde887a286d75c1afdfd957f7b40404
Bluemania <jonghwan0805@gmail.com>
2022-07-15 13:46:57 +0900

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,10 @@ window.addEventListener('DOMContentLoaded', () => {
         return 'formatter >>> ' + value;
       },
       className: 'abs',
+      edit: {
+        show: true,
+        type: 'input'
+      }
     },
     {
       id: 'b',
@@ -49,6 +53,7 @@ window.addEventListener('DOMContentLoaded', () => {
             field: 'bbb1',
             caption: 'bbb1',
             align: 'right',
+            width: 300,
           }, {
             id: 'bbb2',
             field: 'bbb2',
@@ -56,7 +61,11 @@ window.addEventListener('DOMContentLoaded', () => {
           }, {
             id: 'bbb3',
             field: 'bbb3',
-            caption: 'bbb3'
+            caption: 'bbb3',
+            // edit: {
+            //   show: true,
+            //   type: 'input'
+            // }
           }]
         },
         {
@@ -85,11 +94,15 @@ window.addEventListener('DOMContentLoaded', () => {
       caption: 'c',
       formatter: null,
       className: 'c',
-      width: 100,
+      width: 300,
       children: [{
         id: 'cc1',
         field: 'cc1',
         caption: 'cc1',
+        edit: {
+          show: true,
+          type: 'input'
+        }
       }, {
         id: 'cc2',
         field: 'cc2',
@@ -98,6 +111,11 @@ window.addEventListener('DOMContentLoaded', () => {
           id: 'ccc1',
           field: 'ccc1',
           caption: 'ccc1',
+          edit: {
+            show: true,
+            type: 'select',
+            options: [1, 2, 3]
+          }
         }]
       }]
     },
@@ -105,9 +123,27 @@ window.addEventListener('DOMContentLoaded', () => {
       id: 'd',
       field: 'd',
       caption: 'd',
+      edit: {
+        show: true,
+        type: 'input'
+      },
       // captionFormatter 지정시 sorting false 처리
       captionFormatter: function (caption) {
         return `<div>${caption}<button type="button">삭제</button></div>`;
+      },
+    },
+    {
+      id: 'e',
+      field: 'e',
+      caption: 'e'
+    },
+    {
+      id: 'f',
+      field: 'f',
+      caption: 'f',
+      edit: {
+        show: true,
+        type: 'input'
       },
     }
   ];
@@ -147,15 +183,15 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   const config = {
-    theme: 'default', // (String) 'default', 'clean', 'dark'
-    rowHeader: 'no', // (String) 'checkbox', 'no', 'both', 'none',
-    headerGroupFold: true, //(boolean) true, false
+    theme: 'default', // (String) 'default', 'clean', 'dark' 
+    rowHeader: 'checkbox', // (String) 'checkbox', 'no', 'both', 'none',
+    headerGroupFold: false, //(boolean) true, false
     paging: 'client', //(String) 'client', 'server', 'none'
     infiniteScroll: false, // (boolean) true, false ||| infiniteScroll이 false 일 경우에도 data의 길이가 300 이상이라면 true 로 변경됨
     sort: true, // (boolean) true, false
-    viewCount: [10, 20, 30, 50, 100], // (List) Number..
+    viewCount: [10, 20, 30, 50, 100], // (List) Number.. 
     server: {
-      url: 'url',
+      url: '',
       param: {},
       type: 'GET',
     }, // paging : server 일때 (Object)
@@ -232,298 +268,4 @@ window.addEventListener('DOMContentLoaded', () => {
 
 });
 
-*/
-/*
-// compare
-window.addEventListener('DOMContentLoaded', () => {
-  const columns = [{
-      id: 'a',
-      field: 'a',
-      caption: 'a',
-      align: 'left',
-      className: 'abs',
-    },
-    {
-      id: 'b',
-      field: 'b',
-      caption: 'b',
-      formatter: null,
-      className: 'b',
-      children: [{
-          id: 'bb1',
-          field: 'bb1',
-          caption: 'bb1bbbbbbbbbbbbbbbbbbbbb',
-          children: [{
-            id: 'bbb1',
-            field: 'bbb1',
-            caption: 'bbb1',
-            align: 'right',
-          }, {
-            id: 'bbb2',
-            field: 'bbb2',
-            caption: 'bbb2'
-          }, {
-            id: 'bbb3',
-            field: 'bbb3',
-            caption: 'bbb3'
-          }]
-        },
-        {
-          id: 'bb2',
-          field: 'bb2',
-          caption: 'bb2',
-          children: [{
-            id: 'bbb4',
-            field: 'bbb4',
-            caption: 'bbb4'
-          }, {
-            id: 'bbb5',
-            field: 'bbb5',
-            caption: 'bbb5'
-          }, {
-            id: 'bbb6',
-            field: 'bbb6',
-            caption: 'bbb6'
-          }]
-        }
-      ]
-    },
-    {
-      id: 'c',
-      field: 'c',
-      caption: 'c',
-      formatter: null,
-      className: 'c',
-      children: [{
-        id: 'cc1',
-        field: 'cc1',
-        caption: 'cc1',
-      }, {
-        id: 'cc2',
-        field: 'cc2',
-        caption: 'cc2',
-        children: [{
-          id: 'ccc1',
-          field: 'ccc1',
-          caption: 'ccc1',
-        }]
-      }]
-    },
-    {
-      id: 'd',
-      field: 'd',
-      caption: 'd',
-      height: 100,
-    },
-    {
-      id: 'e',
-      field: 'e',
-      caption: 'e',
-    },
-    {
-      id: 'f',
-      field: 'f',
-      caption: 'f',
-    },
-    {
-      id: 'g',
-      field: 'g',
-      caption: 'g',
-    },
-    {
-      id: 'h',
-      field: 'h',
-      caption: 'h',
-    },
-    {
-      id: 'q',
-      field: 'q',
-      caption: 'q',
-    },
-    {
-      id: 'z',
-      field: 'z',
-      caption: 'z',
-    },
-    {
-      id: 'y',
-      field: 'y',
-      caption: 'y',
-    },
-    {
-      id: 'j',
-      field: 'j',
-      caption: 'j',
-    },
-    {
-      id: 'l',
-      field: 'l',
-      caption: 'l',
-    },
-    {
-      id: 'k',
-      field: 'k',
-      caption: 'k',
-    },
-    {
-      id: 'z',
-      field: 'z',
-      caption: 'z',
-    },
-    {
-      id: 'h',
-      field: 'h',
-      caption: 'h',
-    }
-  ];
-
-  const data = [];
-
-  const config2 = {
-    rowHeaderWidth: 80, // (Number)
-    dataColWidth: 200, // (Number)
-    colHeader: {
-      type: 'custom', // (String) no, checkbox, custom
-      height: 85, // (Number)
-      align: 'center', // (String) left, right, center
-    },
-    keyProperty: 'id',
-    valueSeparator: '_rpn',
-    colHeaderFormatter: function (colData, colId, fullData, instance) {
-      return `
-        <div style="width:100%;height:100%;display:flex;flex-direction:column;">
-          <button
-            type="button"
-            style="width:100%;height:25px;"
-            class="col-button"
-            data-col-target="${colId}"
-            data-id="${colData.id}"
-          >
-            X
-          </button>
-          <div style="display:flex;flex-direction:column;">
-            <h4 style="text-align:center;border-bottom:1px solid #cdcdcd;height:30px;line-height:30px;">${colData.id}</h4>
-            <div style="display:flex; height:30px;">
-              <div style="width:50%;border-right:1px solid #cdcdcd;text-align:center;line-height:30px;">검토 내용</div>
-              <div style="width:50%;text-align:center;line-height:30px;">RPN</div>
-            </div>
-          </div>
-        </div>
-      `;
-    },
-    colHeaderHandler: function (data, clickTarget, target, instance) {
-      if (clickTarget.tagName === 'BUTTON') {
-        instance.removeColumn(clickTarget.getAttribute('data-col-target'), data.id);
-      }
-    },
-    loadCallback: function (instance) {}
-  };
-
-  window.compare = new BL.Lib.Compare('grid2', columns, data, config2);
-
-  document.querySelector('#refColumn').addEventListener('click', () => {
-    const ref = [];
-    for (let i = 0; i <= 0; i++) {
-      ref.push({
-        id: `ref`,
-        a: `${i}`,
-        a_rpn: `rpn value ${i}`,
-        bbb1: `${i}`,
-        bbb1_rpn: `rpn value ${i}`,
-        bbb2: `${i}`,
-        bbb2_rpn: `rpn value ${i}`,
-        bbb3: `${i}`,
-        bbb3_rpn: `rpn value ${i}`,
-        bbb4: `${i}`,
-        bbb4_rpn: `rpn value ${i}`,
-        bbb5: `${i}`,
-        bbb5_rpn: `rpn value ${i}`,
-        bbb6: `${i}`,
-        bbb6_rpn: `rpn value ${i}`,
-        bb2: `${i}`,
-        bb2_rpn: `rpn value ${i}`,
-        cc1: `${i}`,
-        cc1_rpn: `rpn value ${i}`,
-        ccc1: `${i}`,
-        ccc1_rpn: `rpn value ${i}`,
-        d: `${i}`,
-        d_rpn: `rpn value ${i}`,
-        ref: true, // 데이터 조회 후 임의로 넣어줘야함. ref 한정
-      })
-    }
-    compare.addColumn(ref, true);
-  })
-
-  document.querySelector('#refColumn2').addEventListener('click', () => {
-    const ref = [];
-    for (let i = 0; i <= 0; i++) {
-      ref.push({
-        id: `ref2`,
-        a: `${i}`,
-        a_rpn: `rpn value ${i}`,
-        bbb1: `${i}`,
-        bbb1_rpn: `rpn value ${i}`,
-        bbb2: `${i}`,
-        bbb2_rpn: `rpn value ${i}`,
-        bbb3: `${i}`,
-        bbb3_rpn: `rpn value ${i}`,
-        bbb4: `${i}`,
-        bbb4_rpn: `rpn value ${i}`,
-        bbb5: `${i}`,
-        bbb5_rpn: `rpn value ${i}`,
-        bbb6: `${i}`,
-        bbb6_rpn: `rpn value ${i}`,
-        bb2: `${i}`,
-        bb2_rpn: `rpn value ${i}`,
-        cc1: `${i}`,
-        cc1_rpn: `rpn value ${i}`,
-        ccc1: `${i}`,
-        ccc1_rpn: `rpn value ${i}`,
-        d: `${i}`,
-        d_rpn: `rpn value ${i}`,
-        ref: true, // 데이터 조회 후 임의로 넣어줘야함. ref 한정
-      })
-    }
-    compare.addColumn(ref, true)
-  })
-
-  document.querySelector('#addColumn').addEventListener('click', () => {
-
-    const data = [];
-    for (let i = 1; i <= 10; i++) {
-      data.push({
-        id: `${i}`,
-        a: `${i}`,
-        a_rpn: `rpn value ${i}`,
-        bbb1: `${i}`,
-        bbb1_rpn: `rpn value ${i}`,
-        bbb2: `${i}`,
-        bbb2_rpn: `rpn value ${i}`,
-        bbb3: `${i}`,
-        bbb3_rpn: `rpn value ${i}`,
-        bbb4: `${i}`,
-        bbb4_rpn: `rpn value ${i}`,
-        bbb5: `${i}`,
-        bbb5_rpn: `rpn value ${i}`,
-        bbb6: `${i}`,
-        bbb6_rpn: `rpn value ${i}`,
-        bb2: `${i}`,
-        bb2_rpn: `rpn value ${i}`,
-        cc1: `${i}`,
-        cc1_rpn: `rpn value ${i}`,
-        ccc1: `${i}`,
-        ccc1_rpn: `rpn value ${i}`,
-        d: `${i}`,
-        d_rpn: `rpn value ${i}`,
-      })
-    }
-
-    compare.addColumn(data);
-  });
-
-  document.querySelector('#excel').addEventListener('click', () => {
-    compare.downloadExcel();
-  })
-});
-
-*/
+ */
